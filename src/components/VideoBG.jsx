e9b97ecc4867b69ed57703a4f81cd74558020047
Sakshi <jsakshi.2707@gmail.com>
2025-07-16 03:56:10 +0530

    embedded trailer, autoplayed &  muted it

--- src/components/VideoBG.jsx
+++ src/components/VideoBG.jsx
@@ -1,37 +1,21 @@
-import React, { useEffect} from 'react'
-import { API_OPTIONS } from '../utils/constants.js'
-import { useDispatch, useSelector } from 'react-redux'
-import { addTrailerVideo } from '../utils/movieSlice.js'
+import React from 'react'
+import { useSelector } from 'react-redux' 
+import useMovieTrailer from "../hooks/useMovieTrailer.js"
 
 const VideoBG = ({ movieId }) => {
 
   const trailerVideo = useSelector(store => store.movies?.trailerVideo)
-const dispatch = useDispatch()
 
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      'https://api.themoviedb.org/3/movie/1011477/videos?language=en-US', 
-      API_OPTIONS
-    )
-    const json = await data.json()
-    console.log(json)
-
-    const filterData = json.results.filter(video => video.type === "Trailer")
-    const trailer = filterData.length ? filterData[0] : json.results[0]
-    console.log(trailer)
-    dispatch((addTrailerVideo(trailer)))
-  }
-
-  useEffect(() => {
-    getMovieVideos()
-  }, []);
+  useMovieTrailer(movieId );
 
   return (
-    <div>
-      <iframe 
+    <div className='w-screen'>
+      <iframe className='w-screen aspect-video'
       width="560" 
       height="315" 
-      src={"https://www.youtube.com/embed/"+trailerVideo?.key}
+      src={
+        "https://www.youtube.com/embed/"+trailerVideo?.key+"?&autoplay=1&mute=1"
+      }
       title="YouTube video player"  
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
       ></iframe>
