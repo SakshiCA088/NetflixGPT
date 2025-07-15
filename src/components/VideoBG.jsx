ad1439fba4d75d61baeecd59daf780916fce6931
Sakshi <jsakshi.2707@gmail.com>
2025-07-16 03:30:06 +0530

    trailer added in redux store

--- src/components/VideoBG.jsx
+++ src/components/VideoBG.jsx
@@ -1,15 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect} from 'react'
 import { API_OPTIONS } from '../utils/constants.js'
+import { useDispatch, useSelector } from 'react-redux'
+import { addTrailerVideo } from '../utils/movieSlice.js'
 
 const VideoBG = ({ movieId }) => {
+
+  const trailerVideo = useSelector(store => store.movies?.trailerVideo)
+const dispatch = useDispatch()
+
   const getMovieVideos = async () => {
-    const data = await fetch('https://api.themoviedb.org/3/movie/1011477/videos?language=en-US', API_OPTIONS)
+    const data = await fetch(
+      'https://api.themoviedb.org/3/movie/1011477/videos?language=en-US', 
+      API_OPTIONS
+    )
     const json = await data.json()
     console.log(json)
 
     const filterData = json.results.filter(video => video.type === "Trailer")
     const trailer = filterData.length ? filterData[0] : json.results[0]
     console.log(trailer)
+    dispatch((addTrailerVideo(trailer)))
   }
 
   useEffect(() => {
@@ -17,7 +27,15 @@ const VideoBG = ({ movieId }) => {
   }, []);
 
   return (
-    <div>VideoBG</div>
+    <div>
+      <iframe 
+      width="560" 
+      height="315" 
+      src={"https://www.youtube.com/embed/"+trailerVideo?.key}
+      title="YouTube video player"  
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      ></iframe>
+    </div>
   )
 }
 
